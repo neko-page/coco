d21ca2c4eb524acf3e80e238a3049870d384c909
鱼喵 <2825924703@qq.com>
2026-04-11 17:15:18 +0800

    Update announcements.js

--- announcements.js
+++ announcements.js
@@ -1,6 +1,6 @@
 // ✅ 正确写法 (announcements.js)
 window.announcements = [
     "欢迎使用 CoCo 快捷导航！",
-    "本站镜像coco.nocode.host，原作者：柠檬桶（垃圾桶）"
+    "本站镜像coco.nocode.host，原作者：柠檬桶（垃圾桶）",
     "我们已修复90%的bug（请忽略剩下的10%）"
 ];
